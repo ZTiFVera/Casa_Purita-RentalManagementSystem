c7bf51e7e0be985e3cbb323d51b5e86e817b21be
akkatske18 <amicus.pradel18@gmail.com>
2026-04-07 17:06:18 +0800

    Edited

---
+++ Casa_Purita-RentalManagementSystem/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Casa_Purita_RentalManagementSystem.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Server=LAB4-PC35\\LAB2PC31;Database=CasaPuritaDb;Integrated Security=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
